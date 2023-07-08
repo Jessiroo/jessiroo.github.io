294746c59ff30f7704846904fedb8867a61d20c3
Jessiroo <jessie.madsen@gmail.com>
2023-07-08 16:33:49 -0600

    update links

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,5 +1,3 @@
-import { Link, useNavigate } from "react-router-dom";
-
 import ContentCard from "../Layout/ContentCard";
 import Divider from "../Layout/Divider";
 import InLogo from '../assets/LI-In-Bug.png';
@@ -8,11 +6,6 @@ import ContactForm from "../components/ContactForm";
 import classes from './Contact.module.css';
 
 const ContactPage = () => {
-  const navigate = useNavigate();
-
-  const linkClickHandler = (url) => {
-    navigate(url);
-  };
 
   return (
     <ContentCard>
@@ -21,28 +14,38 @@ const ContactPage = () => {
       <section className={classes.contactLinks}>
         <div className={classes.contactOption}>
           <div className={classes.contactLogo}>
-            <img 
-              src={InLogo}
-              className={classes.logos}
-              alt="LinkedIn logo and link" 
-              onClick={linkClickHandler.bind(null, '/')}
-            />
+            <a 
+              href="/" 
+              target="_blank"
+              rel="external"
+            >
+              <img 
+                src={InLogo}
+                className={classes.logos}
+                alt="LinkedIn logo and link" 
+              />
+            </a>
           </div>
           <div className={classes.contactText}>
-            <p>View my profile on <Link to="/">LinkedIn<sup className={classes.superscript}>&reg;</sup></Link></p>
+            <p>View my profile on <a href="/" target="_blank" rel="external">LinkedIn<sup className={classes.superscript}>&reg;</sup></a></p>
           </div>
         </div>
         <div className={classes.contactOption}>
           <div className={classes.contactLogo}>
-            <img 
-              src={GitHubLogo}
-              className={classes.logos}
-              alt="GitHub logo and link" 
-              onClick={linkClickHandler.bind(null, 'https://github.com/Jessiroo')}
-            /> 
+            <a 
+              href="https://github.com/Jessiroo" 
+              target="_blank"
+              rel="external"
+            >
+              <img 
+                src={GitHubLogo}
+                className={classes.logos}
+                alt="GitHub logo and link" 
+              /> 
+            </a>
           </div>
           <div className={classes.contactText}>
-            <p>Check me out on <Link to="https://github.com/Jessiroo">GitHub<sup className={classes.superscript}>&reg;</sup></Link></p>
+            <p>Check me out on <a href="https://github.com/Jessiroo" target="_blank" rel="external">GitHub<sup className={classes.superscript}>&reg;</sup></a></p>
           </div>
         </div>
       </section>
--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -23,34 +23,60 @@ const PortfolioPage = () => {
       <p>Please check out some of the projects I have worked on. Each includes a link to the hosted site and the GitHub repo.</p>
       <section>
         <div className={classes.portfolioPiece}>
-          <Link to="https://bright-cranachan-cecabf.netlify.app/"><h2>Happy Paws</h2></Link>
-          <div className={classes.portfolioPieceImage}>
+          <a 
+            href="https://bright-cranachan-cecabf.netlify.app/"
+            target="_blank"
+            rel="external"
+            className={classes.link}
+          ><h2>Happy Paws</h2></a>
+          <a 
+            href="https://bright-cranachan-cecabf.netlify.app/"
+            target="_blank"
+            rel="external"
+            className={classes.portfolioPieceImage}
+          >
             <img 
               src={HappyPaws} 
-              onClick={linkClickHandler.bind(null, "https://bright-cranachan-cecabf.netlify.app/")}
               alt='Pet adoption website homepage.' 
             />
-          </div>
+          </a>
           <p>This was the first solo project I worked on. It was originally intended to be a small project to practice with, but it quickly grew as I kept coming up with new ideas to expand upon it with new features and functionalities. It has fetch and post requests, makes use of React Router, has a featured carousel, filtering, custom hooks, context, and more...</p>
-          <GitHubMark 
-            onClick={linkClickHandler.bind(null, "https://github.com/Jessiroo/happy-paws")} 
-          />
+          <a 
+            href="https://github.com/Jessiroo/happy-paws"
+            target="_blank"
+            rel="external"
+          >
+            <GitHubMark />
+          </a>
         </div>
         <div className={classes.portfolioDivider}/>
         <div className={classes.portfolioPiece}>
-          <Link to="https://effervescent-choux-5431b3.netlify.app/"><h2>React Meals</h2></Link>
-          <div className={classes.portfolioPieceImage}>
+          <a 
+            href="https://effervescent-choux-5431b3.netlify.app/"
+            target="_blank"
+            rel="external"
+            className={classes.link}
+          ><h2>React Meals</h2></a>
+          <a 
+            href="https://effervescent-choux-5431b3.netlify.app/"
+            target="_blank"
+            rel="external"
+            className={classes.portfolioPieceImage}
+          >
             <img 
               src={ReactMeals} 
               className={classes.portfolioPieceImage}
-              onClick={linkClickHandler.bind(null, "https://effervescent-choux-5431b3.netlify.app/")}
               alt='React Meals website homepage.' 
             />
-          </div>
+          </a>
           <p>This was one of the projects I worked on while doing Maximilian Schwarzmüller's course: React - The Complete Guide. In order to maximize my learning from the course I would frequently pause the video and code ahead of the instructor. During large projects like this, I would look ahead to see what the desired end-product looked like and attempt to re-create it ahead of time. Once finished, I would start another project and code along with the instructor to get more practice and see how our solutions differed. This was my code-ahead version of this project.</p>
-          <GitHubMark 
-            onClick={linkClickHandler.bind(null, "https://github.com/Jessiroo/react-meals")} 
-          />
+          <a 
+            href="https://github.com/Jessiroo/react-meals"
+            target="_blank"
+            rel="external"
+          >
+            <GitHubMark />
+          </a>
         </div>
         <div className={classes.portfolioDivider}/>
         <div className={classes.portfolioPiece}>
