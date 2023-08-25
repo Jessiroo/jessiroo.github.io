f4425fa33e7b4b459f1ad879566ff8c6f6dfa8a8
Jessiroo <jessie.madsen@gmail.com>
2023-08-24 22:54:40 -0600

    tweaking background

--- src/pages/Root.js
+++ src/pages/Root.js
@@ -1,6 +1,7 @@
 import { Fragment } from 'react';
 import { Outlet } from 'react-router-dom';
 
+import Backdrop from '../Layout/Backdrop';
 import Navbar from '../Layout/Navbar';
 
 const RootLayout = () => {  
@@ -8,6 +9,7 @@ const RootLayout = () => {
     <Fragment >
       <Navbar />
       <Outlet />
+      <Backdrop />
     </Fragment>
   );
 };
