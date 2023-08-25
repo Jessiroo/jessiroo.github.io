a05e7f9200ec24d007468b9d38459e6becf721af
Jessiroo <jessie.madsen@gmail.com>
2023-08-24 22:31:59 -0600

    fixing background

--- src/pages/Root.js
+++ src/pages/Root.js
@@ -1,15 +1,15 @@
 import { Fragment } from 'react';
 import { Outlet } from 'react-router-dom';
 
-import Backdrop from '../Layout/Backdrop';
+// import Backdrop from '../Layout/Backdrop';
 import Navbar from '../Layout/Navbar';
 
 const RootLayout = () => {  
   return (
-    <Fragment>
+    <Fragment >
       <Navbar />
       <Outlet />
-      <Backdrop />
+      {/* <Backdrop /> */}
     </Fragment>
   );
 };
